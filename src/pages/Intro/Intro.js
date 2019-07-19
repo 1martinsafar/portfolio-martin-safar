11d680086d0a64487b86bdc5b4e23d7aa06747d7
Martin Safar <1martinsafar@gmail.com>
2019-07-19 21:50:24 +0200

    Pages moved to their own folders

---
+++ src/pages/Intro/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Intro.module.css';
+
+const Intro = () => {
+  return (
+    <div>Intro PAGE</div>
+  );
+}
+
+export default Intro;
---
+++ src/pages/Portfolio/Portfolio.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Portfolio.module.css';
+
+const Portfolio = () => {
+  return (
+    <div>Portfolio PAGE</div>
+  );
+}
+
+export default Portfolio;
