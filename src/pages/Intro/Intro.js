605bff0ad1134aba35d335b99499e6f737b44e6b
Martin Safar <1martinsafar@gmail.com>
2019-07-20 20:03:57 +0200

    Introduction page completed mobile/desktop

--- src/pages/Intro/Intro.js
+++ src/pages/Intro/Intro.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import css from './Intro.module.css';
 
 const Intro = () => {
-  return (
-    <div>Intro PAGE</div>
-  );
-}
+    return (
+        <div className={css.Intro}>
+            <div className={css.Content}>
+                <h1>Hi, I'm Martin.</h1>
+                <h2>Here to check out my portfolio?</h2>
+                <NavLink to="/portfolio" className={css.Link}>
+                    RIGHT THIS WAY!
+                </NavLink>
+            </div>
+        </div>
+    );
+};
 
 export default Intro;
