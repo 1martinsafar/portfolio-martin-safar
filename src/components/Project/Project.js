8bb8af2bb5f85699009367cff172b4bbe89badf5
Martin Safar <1martinsafar@gmail.com>
2019-07-22 18:30:24 +0200

    Portfolio page and Project component for mobile created

---
+++ src/components/Project/Project.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import css from './Project.module.css';
+
+import Heading from '../UI/Heading/Heading';
+import Text from '../UI/Text/Text';
+import Link from '../UI/Link/Link';
+
+const Project = props => {
+    const image = props.image;
+    const heading = props.heading;
+    const linkCheck = props.linkCheck;
+    const linkFiles = props.linkFiles;
+    const skills = props.skills;
+    const description = props.description;
+
+    return (
+        <div className={css.Project}>
+            <div className={css.PictureFrame}>
+                <img src={image} alt="Portfolio Project" />
+            </div>
+            <Heading smallMargin smallFont>
+                {heading}
+            </Heading>
+            <div className={css.Row}>
+                {linkCheck && <Link solo href={linkCheck}>CHECK ME OUT</Link>}
+                {linkFiles && <Link solo right href={linkFiles}>PROJECT FILES</Link>}
+            </div>
+            <Heading smallMargin smallFont>
+                SKILLS USED
+            </Heading>
+            <Text>
+                {skills}
+            </Text>
+            <Heading smallMargin smallFont>
+                DESCRIPTION
+            </Heading>
+            <Text>
+                {description}
+            </Text>
+        </div>
+    );
+};
+
+export default Project;
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import css from './Portfolio.module.css';
 
+import Project from '../../components/Project/Project';
+import projectData from '../../project-data/projectData';
+
 const Portfolio = () => {
-  return (
-    <div>Portfolio PAGE</div>
-  );
-}
+
+
+    return (
+        <div className={css.Portfolio}>
+            <div className={css.Wrapper}>
+                {projectData.reverse().map((data, i) => {
+                    return (
+                        <Project
+                            image={data.image}
+                            heading={data.heading}
+                            linkCheck={data.linkCheck}
+                            linkFiles={data.linkFiles}
+                            skills={data.skills}
+                            description={data.description}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
 
 export default Portfolio;
