5dfcf42770aafd7b53a5c5bd4c44e7534bca5197
Martin Safar <1martinsafar@gmail.com>
2022-09-21 21:42:44 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/1martinsafar/portfolio-martin-safar/precache-manifest.0b7e53174e86d7b3652fd93c87b4efae.js"
+  "/precache-manifest.ea25d999c8841982cfbdb5de570eb3ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/1martinsafar/portfolio-martin-safar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
